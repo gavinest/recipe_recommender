407e196125e20655b02a632cbe52a7acabf8200a
saoce <gav.estenssoro@gmail.com>
2016-10-31 18:02:05 -0600

    add tokenization & mongo updating

--- data/deals/scrape/scrape_ibotta.py
+++ data/deals/scrape/scrape_ibotta.py
@@ -15,14 +15,14 @@ class IbottaScraper(object):
     '''
     class to get grocery offer data from Ibotta.com. Processes non-duplicated offers to MongoDB.
 
-    If class does not work you may need to get new 'X-App-Token' or 'Cookie' by:
+    To get App_Token:
         1. go to ibotta.com and filter by grocery
         2. With google dev tools
             a. right click -> inspect -> network -> XHR -> refresh page
             b. click to open details of 'offer.json'
-            c. scroll to 'request-headers' section. replace 'X-App-Token' and 'Cookie' of class 'self.header' with new values in 'request-headers' section.
+            c. scroll to 'request-headers' section. replace 'X-App-Token' of class 'self.header' with new values in 'request-headers' section.
     '''
-    def __init__(self):
+    def __init__(self, App_Token):
         self.time = time.time()
         #url to .json file which is loaded via ajax
         self.url = 'https://ibotta.com/web_v1/offers.json'
@@ -34,17 +34,16 @@ class IbottaScraper(object):
             'Accept': 'application/json, text/javascript, */*; q=0.01',
             'X-App-Version': '4.9.4:webapp',
             'X-Requested-With': 'XMLHttpRequest',
-            'X-App-Token': 'lyWreHoghkuWyed6:1477942768:395b08062357d3806934b6b1d3f51fb951e56c3a',
+            'X-App-Token': App_Token,
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
             'Referer': 'https://ibotta.com/rebates?category=grocery',
             'Accept-Encoding': 'gzip, deflate, sdch, br',
             'Accept-Language': 'en-US,en;q=0.8',
             'Cookie': '__ssid=4ddda54d-af1a-4883-8f3d-648adc887fe3; viewedOuibounceModal=true; _ga=GA1.2.2058757665.1472082122; _gat=1; ab.storage.sessionId.cb535ba1-2856-474b-9f5d-1416c922440d=%7B%22g%22%3A%22511b896c-b4fd-a476-7de2-97fce85e79cd%22%2C%22e%22%3A1477944569441%2C%22c%22%3A1477942769442%2C%22l%22%3A1477942769442%7D',
-            'If-Modified-Since': 'Mon, 31 Oct 2016 19:00:00 GMT',
+            'If-Modified-Since': 'Mon, 31 Oct 2016 5:00:00 GMT',
             }
         #categories that are of interest for recipes
         self.offer_categories = {
-            'Spend & Earn' : 426,
             'Pantry' : 450,
             'Frozen' : 13,
             'Beverages' : 1,
@@ -63,7 +62,6 @@ class IbottaScraper(object):
             'Bread & Bakery' : 415,
             }
         self._scrape()
-        self._process()
 
     def _scrape(self):
         r = requests.get(self.url,
@@ -71,6 +69,7 @@ class IbottaScraper(object):
         if r.status_code == 200 or r.status_code == 304: #304 means data not modified
             print 'Success! status code', r.status_code
             self.data = json.loads(r.text)
+            self._process()
         else:
             print 'Error: status code ', r.status_code
 
@@ -79,7 +78,10 @@ class IbottaScraper(object):
             for cat_id in offer['offer_category_ids']:
                 if cat_id in self.offer_categories.values():
                     if not bool(COLLECTION.find({'id': offer['id']}).count()):
+                        offer['tags'] = [t['offer_tag']for t in offer['offer_tags']]
                         COLLECTION.insert_one(offer)
 
 if __name__ == '__main__':
-    ibotta = IbottaScraper()
+    token = 'lyWreHoghkuWyed6:1477951824:d3e4bbfdc5ac47b07348f533a5c7119ae7a5551a'
+    #App_Tokens expire when browser session is closed so need to get new one at least daily
+    ibotta = IbottaScraper(App_Token=token)
---
+++ data/recipes/clean/clean_recipes.py
@@ -0,0 +1,56 @@
+from pymongo import MongoClient
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from string import punctuation
+from unidecode import unidecode
+
+#terminal command for duplicating mongodb for testing updating
+# mongoexport -d db -c sourcecollection | mongoimport -d db -c targetcollection --drop
+# i.e. mongoexport -d allrecipes -c recipes | mongoimport -d allrecipes -c test --drop
+
+#global variables
+DB_NAME = 'allrecipes'
+COLL_NAME = 'recipes'
+CLIENT = MongoClient()
+DATABASE = CLIENT[DB_NAME]
+COLLECTION = DATABASE[COLL_NAME]
+
+def stop_words():
+    recipe_stopwords = set(['pound', 'pounds', 'tablespoon', 'tablespoons', 'teaspoon', 'teaspoons', 'cup', 'cups', 'bunch', 'chopped'])
+    stop = set(stopwords.words('english'))
+    return stop.union(recipe_stopwords)
+
+def clean_ingredients(recipe):
+    stop = stop_words()
+    cleaned = []
+    for line in recipe:
+        line = [_ for _ in unidecode(line).lower().translate(None, punctuation).split() if not _.isdigit()]
+        lemmers = [WordNetLemmatizer().lemmatize(word) for word in line if word not in stop]
+        cleaned.append([unidecode(word) for word in lemmers])
+    return cleaned
+
+def update_recipes():
+    for recipe in COLLECTION.find():
+        COLLECTION.update_one({'_id': recipe['_id']},
+                {'$set': {'tokens': clean_ingredients(recipe['ingredients'])}},
+                upsert=False)
+        # print clean_ingredients(recipe['ingredients'])
+    print 'Updated!'
+
+if __name__ == '__main__':
+    test_list = [
+		"6 pounds roma (plum) tomatoes",
+		"1/4 pound roma (plum) tomatoes, chopped",
+		"2 tablespoons garlic powder",
+		"1/4 cup lemon juice",
+		"1 1/2 tablespoons salt",
+		"1 tablespoon ground cayenne pepper",
+		"1 1/2 teaspoons ground cumin",
+		"1 red onion, chopped",
+		"1 white onion, chopped",
+		"1 yellow onion, chopped",
+		"1 pound jalapeno peppers, chopped",
+		"1/3 bunch fresh cilantro, chopped"
+	     ]
+    # t = clean_ingredients(test_list)
+    update_recipes()
