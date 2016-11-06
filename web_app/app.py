1525b78fd06caff19977cb08e7a74849d67ca43c
saoce <gav.estenssoro@gmail.com>
2016-11-05 23:23:57 -0600

    many web_app changes

--- web_app/app.py
+++ web_app/app.py
@@ -25,54 +25,27 @@ def hello_world():
 def contact_me():
     return render_template('contact.html')
 
-@app.route('/recommend', methods=["GET","POST"])
-# @app.route('/recommend/<user_id>')
-# def recommender(user_id=827351): #827351
-def recommender():
+@app.route('/recommender/<user_id>')
+def recommender(user_id):
+    recommendations = model.recommend([user_id], k=5)['recipe_id']
+    recipe_cards =[]
+    for recipe in recommendations:
+        recipe_cards.append(RECIPE_COLLECTION.find_one({'recipe_id': str(recipe)}))
+    return render_template('recommender.html', cards=recipe_cards)
+
+@app.route('/login', methods=["GET", "POST"])
+@app.route('/recommend')
+@app.route('/recommender')
+#827351
+def login():
+    error = ''
     if request.method == "POST":
         user_id = request.form['user_id']
         if USER_COLLECTION.find({'user_id': user_id}).count() != 0:
-            rec = model.recommend([945781], k=5)[0]['recipe_id']
-            print rec
-            recipe_card = data.pull_recipe_data([str(rec)])[0]
-            return render_template('recommender.html',
-                        recipe_name=recipe_card['name'],
-                        ingredients=recipe_card['ingredients'],
-                        directions=recipe_card['directions'])
-    else:
-        rec = model.recommend([827351], k=5)[0]['recipe_id']
-        recipe_card = data.pull_recipe_data([str(rec)])[0]
-        return render_template('recommender.html',
-                    recipe_name=recipe_card['name'],
-                    ingredients=recipe_card['ingredients'],
-                    directions=recipe_card['directions'])
-
-    # @app.route('/login/', methods=["GET","POST"])
-    # def login_page():
-    #
-    #     error = ''
-    #     try:
-    #
-    #         if request.method == "POST":
-    #
-    #             attempted_username = request.form['username']
-    #             attempted_password = request.form['password']
-    #
-    #             #flash(attempted_username)
-    #             #flash(attempted_password)
-    #
-    #             if attempted_username == "admin" and attempted_password == "password":
-    #                 return redirect(url_for('dashboard'))
-    #
-    #             else:
-    #                 error = "Invalid credentials. Try Again."
-    #
-    #         return render_template("login.html", error = error)
-    #
-    #     except Exception as e:
-    #         #flash(e)
-    #         return render_template("login.html", error = error)
-    #
+            return redirect(url_for('recommender', user_id=user_id))
+        else:
+            error = 'Invalid User ID. Do you want to register?'
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
     data = DataLoader(10)
