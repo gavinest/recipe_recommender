7f3741b8ee1ad154becc67cdca484cec7b1024ed
saoce <gav.estenssoro@gmail.com>
2016-11-11 00:13:20 -0700

    add registration form

--- web_app/app.py
+++ web_app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, url_for, redirect
+from wtforms import Form
 import numpy as np
 import pandas as pd
 import graphlab as gl
@@ -16,23 +17,41 @@ DATABASE = CLIENT[DB_NAME]
 RECIPE_COLLECTION = DATABASE['recipes']
 USER_COLLECTION = DATABASE['users']
 
-#classes
 # class RegistrationForm(Form):
-#     username = TextField('Username', [validators.Length(min=4, max=20)])
-#     email = TextField('Email Address', [validators.Length(min=6, max=50)])
-#     password = PasswordField('New Password', [
-#         validators.Required(),
-#         validators.EqualTo('confirm', message='Passwords must match')
-#     ])
-#     confirm = PasswordField('Repeat Password')
-#     accept_tos = BooleanField('I accept the Terms of Service and Privacy Notice (updated Jan 22, 2015)', [validators.Required()])
-#
+    # username = TextField('Username', [validators.Length(min=4, max=20)])
+    # email = TextField('Email Address', [validators.Length(min=6, max=50)])
+    # password = PasswordField('New Password', [
+    #     validators.Required(),
+    #     validators.EqualTo('confirm', message='Passwords must match')
+    # ])
+    # confirm = PasswordField('Repeat Password')
+    # accept_tos = BooleanField('I accept the Terms of Service and Privacy Notice (updated Jan 22, 2015)', [validators.Required()])
 
 
-@app.route('/')
-@app.route('/about')
-def hello_world():
-    return render_template('summary.html', intro_header='Recipe Recommender', tag_line='Make Good Food. Save Money.')
+@app.route('/login', methods=["GET", "POST"])
+@app.route('/recommend', methods=["GET", "POST"])
+@app.route('/recommender', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
+#827351
+def login():
+    error = None
+    if request.method == "POST":
+
+        if 'user_id' in request.form:
+            user = request.form['user_id']
+            if USER_COLLECTION.find({'user_id': user}).count() != 0:
+                return redirect(url_for('recommender', user_id=user))
+            else:
+                error = True
+        elif 'register' in request.form:
+            new_user = request.form['register']
+            print 'weeeeeeee'
+    return render_template('login.html', intro_header='The Recipe Recommender',  tag_line='Delicious Food is Just a Click Away', error=error, random_users=get_random_ids())
+
+
+# @app.route('/about')
+# def hello_world():
+#     return render_template('summary.html', intro_header='Recipe Recommender', tag_line='Make Good Food. Save Money.')
 
 @app.route('/contact')
 def contact_me():
@@ -56,20 +75,6 @@ def recommender(user_id):
     for recipe in recommendations:
         recipe_cards.append(RECIPE_COLLECTION.find_one({'recipe_id': str(recipe)}))
     return render_template('recommender.html', cards=recipe_cards)
-
-@app.route('/login', methods=["GET", "POST"])
-@app.route('/recommend', methods=["GET", "POST"])
-@app.route('/recommender', methods=["GET", "POST"])
-#827351
-def login():
-    error = None
-    if request.method == "POST":
-        user_id = request.form['user_id']
-        if USER_COLLECTION.find({'user_id': user_id}).count() != 0:
-            return redirect(url_for('recommender', user_id=user_id))
-        else:
-            error = True
-    return render_template('login.html', intro_header='The Recommender',  tag_line='Delicious Food is Just a Click Away', error=error, random_users=get_random_ids())
 
 def get_random_ids():
     s = list(set(df['user_id'].values))
